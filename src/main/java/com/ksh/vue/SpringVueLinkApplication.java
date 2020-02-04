4cb9fc393bf63060abda0ecf47bad7a481574d99
Kimsihwan <namid882@gmail.com>
2020-02-04 15:24:28 +0900

    add front & backend

---
+++ src/main/java/com/ksh/vue/SpringVueLinkApplication.java
@@ -0,0 +1,13 @@
+package com.ksh.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVueLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksh/vue/SpringVueLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
